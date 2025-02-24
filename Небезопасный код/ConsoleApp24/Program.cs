6ae267ae53f5816414aec8aa2faceae931a1d645
POMOR2021 <93044445+POMOR2021@users.noreply.github.com>
2025-02-24 21:11:39 +0300

    Add files via upload

---
+++ Небезопасный код/ConsoleApp24/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UnsafeArrayCopy
+{
+    class Program
+    {
+        static unsafe void CopyArrayUnsafe(int[] sour, int[] dest)
+        {
+            if (sour == null || dest == null || sour.Length > dest.Length)
+            {
+                throw new ArgumentException("Ложные аргументы.");
+            }
+
+            fixed (int* sourPt = sour)
+            fixed (int* destPt = dest)
+            {
+                for (int i = 0; i < sour.Length; i++)
+                {
+                    *(destPt + i) = *(sourPt + i);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] sourArr = { 1, 2, 3, 4, 5 };
+            int[] destArr = new int[10]; 
+
+            try
+            {
+                unsafe
+                {
+                    CopyArrayUnsafe(sourArr, destArr);
+                }
+
+                Console.WriteLine("Copy massive:");
+                foreach (int val in destArr)
+                {
+                    Console.Write(val + " ");
+                }
+                Console.WriteLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
