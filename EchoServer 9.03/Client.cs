4ca614983bbbd290a86836cd500cc3bd37219990
POMOR2021 <93044445+POMOR2021@users.noreply.github.com>
2025-07-01 18:14:29 +0300

    Add files via upload

---
+++ EchoServer 9.03/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        var ip = "127.0.0.1";
+        var port = 8888;
+
+        try
+        {
+            using (var cli = new TcpClient(ip, port))
+            using (var stream = cli.GetStream())
+            {
+                Console.WriteLine($"Подключено к {ip}:{port}");
+                Console.WriteLine("Вводите текст (exit для выхода):");
+
+                while (true)
+                {
+                    Console.Write("> ");
+                    var msg = Console.ReadLine();
+
+                    if (msg.ToLower() == "exit")
+                        break;
+
+                    var data = Encoding.UTF8.GetBytes(msg);
+                    stream.Write(data, 0, data.Length);
+
+                    var buf = new byte[1024];
+                    var cnt = stream.Read(buf, 0, buf.Length);
+                    var res = Encoding.UTF8.GetString(buf, 0, cnt);
+                    Console.WriteLine($"Ответ: {res}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ EchoServer 9.03/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        var ip = IPAddress.Parse("127.0.0.1");
+        var port = 8888;
+
+        var srv = new TcpListener(ip, port);
+
+        try
+        {
+            srv.Start();
+            Console.WriteLine($"Сервер запущен {ip}:{port}");
+
+            while (true)
+            {
+                using (var client = srv.AcceptTcpClient())
+                using (var stream = client.GetStream())
+                {
+                    Console.WriteLine($"Клиент подключен");
+
+                    var buf = new byte[1024];
+                    int cnt;
+
+                    while ((cnt = stream.Read(buf, 0, buf.Length)) > 0)
+                    {
+                        var msg = Encoding.UTF8.GetString(buf, 0, cnt);
+                        Console.WriteLine($"Получено: {msg}");
+
+                        var res = Encoding.UTF8.GetBytes(msg);
+                        stream.Write(res, 0, res.Length);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+        finally
+        {
+            srv.Stop();
+        }
+    }
+}
